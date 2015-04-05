3b535862546cb3e4cdc2c39c7d687205d936821a
ali <aliahmadmohdi@gmail.com>
2015-04-06 04:16:57 +0530

    a

--- project3/wsgi.py
+++ project3/wsgi.py
@@ -13,4 +13,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project3.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application
+application = DjangoWhiteNoise(application)
